2a4cc30d55e39aa0a0ab934601225e3b97079582
hognod <125235349+hognod@users.noreply.github.com>
2025-01-16 17:26:35 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,10 @@
 variable "ami_id" {
   type = string
-  # default = "ami-09eb4311cbaecf89d"
-  # description = "ubuntu 20.04"
-  # default = "ami-05d2438ca66594916"
-  # description = "ubuntu 22.04"
-  default = "ami-06fc9949c7e49967a"
-  description = "RHEL_HA-8.4.0_HVM-20230419-x86_64-41-Hourly2-GP2"
+  description = <<-EOT
+    ubuntu 20.04 : ami-09eb4311cbaecf89d
+    ubuntu 22.04 : ami-05d2438ca66594916
+    RHEL_HA-8.4.0_HVM-20230419-x86_64-41-Hourly2-GP2 : ami-06fc9949c7e49967a
+  EOT
 }
 
 variable "instance_type" {
