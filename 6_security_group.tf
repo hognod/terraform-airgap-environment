3d2d2ada9431631be42259aed5450175aa0989ab
hognod <125235349+hognod@users.noreply.github.com>
2025-06-02 17:57:50 +0900

    Update 6_security_group.tf

--- 6_security_group.tf
+++ 6_security_group.tf
@@ -22,15 +22,6 @@ resource "aws_security_group" "public" {
   }
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "TCP"
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-  }
-
-  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "TCP"
